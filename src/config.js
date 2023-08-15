6f3d87bbb5fa23e0a8240613cfb61ee2a36e5d95
Vladislav <vladi.harizanov@gmail.com>
2023-08-15 10:41:20 +0300

    Connected to fastapi server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_BASE_URL = 'http://127.0.0.1:8000/'; // 
+const API_BASE_URL = 'https://fastapideta-1-a1742038.deta.app/'; // https://fastapideta-1-a1742038.deta.app/
 
 export default API_BASE_URL;
